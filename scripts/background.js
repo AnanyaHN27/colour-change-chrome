5f3f8cabe43fac4039ee2c51295333d7d0f60b86
Ananya Hari Narain <ananya.harinarain@gmail.com>
2023-02-10 13:49:32 +0000

    typo fix

---
+++ scripts/background.js
@@ -0,0 +1,25 @@
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab){
+    if (changeInfo.status === "complete"){
+        chrome.storage.sync.get("color", function(data){
+            if (data["color"]){
+                chrome.tabs.executeScript(
+                    tabId,
+                    {code: 'document.body.style.color = ' + data["color"] + ";"}
+                )
+            }
+        })
+    }
+})
+
+chrome.runtime.onInstalled.addListener(function() {
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
