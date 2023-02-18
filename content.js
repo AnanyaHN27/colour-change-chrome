a64dfa0f05690c1cbc70e02d400405424eb53496
Ananya Hari Narain <ananya.harinarain@gmail.com>
2023-02-18 10:08:56 +0000

    working commit

--- content.js
+++ content.js
@@ -1,37 +1,27 @@
-const redButton = document.getElementById('redButton');
-const blueButton = document.getElementById('blueButton')
+document.addEventListener("DOMContentLoaded", function (){
+    const redBtn = document.getElementById('nameRed');
+    const blueBtn = document.getElementById('nameBlue');
+    if (redBtn){
+        console.log("red exists");
+        redBtn.addEventListener("click", function (){
+            document.body.innerText.fontcolor("red")
 
+            chrome.tabs.insertCSS({
+                code: ` *{
+                color: red;
+            }`
+            })
+        })
+    }
+    if (blueBtn){
+        console.log("blue exists")
+        blueBtn.addEventListener("click", function (){
+            chrome.tabs.insertCSS({
+                code: ` *{
+                color: blue;
+            }`
+            })
+        })
+    }
 
-
-redButton.addEventListener("click", function onClick(){
-    let buttonColour = redButton.style.color;
-    colourChange(buttonColour)
 })
-
-function colourChange(colour){
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.color = ' + colour + ';'}
-        );
-    });
-    storeColour(colour);
-}
-
-function storeColour(colour){
-    chrome.storage.sync.set({color: colour});
-}
-
-/*
-document.body.addEventListener('click', function (evt){
-   let target = evt.target;
-   if (target.nodeName === 'BUTTON'){
-       chrome.storage.sync.clear(function (data){
-           chrome.tabs.query({active: true, currentWindow: true}, function (tab){
-               chrome.tabs.reload(tab[0].id)
-           });
-       });
-       colourChange(target)
-   }
-});
-*/
